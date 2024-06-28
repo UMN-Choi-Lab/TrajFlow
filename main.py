4e52d02e53f3b8ec3875dd626208801f9ac315b9
kosieram21 <mitchkos21@gmail.com>
2024-06-28 17:11:35 -0500

    script cleanup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from TrajCNF import TrajCNF
 
 # hyper parameters
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+verbose = True
 
 # model parameters
 seq_len = 100
@@ -33,34 +34,35 @@ traj_flow = TrajCNF(seq_len=seq_len, input_dim=input_dim, feature_dim=feature_di
 # cnf train
 traj_flow.train()
 if train:
-   optim = torch.optim.Adam(traj_flow.parameters(), lr=lr)
-   scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, scheduler_gamma)
-   total_loss = []
-   for epoch in range(epochs):
-       losses = []
-       for inputs, features in dm.train_loader:
-           input = inputs[:, :100, ...].to(device)
-           target = inputs[:, 100:, ...].to(device)
-           features = features[:, :100, ...].to(device)
+    optim = torch.optim.Adam(traj_flow.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, scheduler_gamma)
+    total_loss = []
+    for epoch in range(epochs):
+        losses = []
+        for inputs, features in dm.train_loader:
+            input = inputs[:, :100, ...].to(device)
+            target = inputs[:, 100:, ...].to(device)
+            features = features[:, :100, ...].to(device)
 
-           z_t0, delta_logpz = traj_flow(input, target, features)
-           logpz_t0, logpz_t1 = traj_flow.log_prob(z_t0, delta_logpz)
-           loss = -torch.mean(logpz_t1)
+            z_t0, delta_logpz = traj_flow(input, target, features)
+            logpz_t0, logpz_t1 = traj_flow.log_prob(z_t0, delta_logpz)
+            loss = -torch.mean(logpz_t1)
 
-           print(f'logpz_t0 (latent): {-torch.mean(logpz_t0)}')
-           print(f'logpz_t1 (prior): {loss}')
+            if verbose:
+                print(f'logpz_t0 (latent): {-torch.mean(logpz_t0)}')
+                print(f'logpz_t1 (prior): {loss}')
 
-           total_loss.append(loss.item())
-           optim.zero_grad()
-           loss.backward()
-           optim.step()
-           scheduler.step()
-           losses.append(loss.item())
-       print(f"epoch: {epoch}, loss: {np.mean(losses):.4f}")
+            total_loss.append(loss.item())
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            scheduler.step()
+            losses.append(loss.item())
+        print(f"epoch: {epoch}, loss: {np.mean(losses):.4f}")
 
-   plt.plot(total_loss)
+    plt.plot(total_loss)
 
-   torch.save(traj_flow.state_dict(), 'traj_cnf.pt')
+    torch.save(traj_flow.state_dict(), 'traj_cnf.pt')
 
 traj_flow.load_state_dict(torch.load('traj_cnf.pt'))
 
