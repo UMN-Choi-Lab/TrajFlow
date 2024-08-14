f61c0495e98512084fbf4578c950840962d8abb8
kosieram21 <mitchkos21@gmail.com>
2024-08-14 14:05:21 -0500

    enable gru imputation

--- model/encoder/GRU.py
+++ model/encoder/GRU.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 class GRU(nn.Module):
 	def __init__(self, input_dim, hidden_dim, num_layers):
 		super(GRU, self).__init__()
-		#self.gru = nn.GRU(input_dim + 2, hidden_dim, num_layers, batch_first=True)
-		self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+		self.gru = nn.GRU(input_dim + 2, hidden_dim, num_layers, batch_first=True)
+		#self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
 
 	def forward(self, t, x):
-		#imputed = self._forward_imputation(t, x)
-		imputed = x	
+		imputed = self._forward_imputation(t, x)
+		#imputed = x	
 		embedding, _ = self.gru(imputed)
 		return embedding[:, -1, :]
 	
